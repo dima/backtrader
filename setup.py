5b1e6c51ee7e0871f2d7832e32c8547a55f97626
Dima Berastau <dima.berastau@gmail.com>
2020-07-04 23:31:53 -0700

    Specific bokeh requirement for now.

--- setup.py
+++ setup.py
@@ -118,7 +118,7 @@ setuptools.setup(
     # You can install these using the following syntax, for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'plotting':  ['matplotlib', 'bokeh', 'jinja2', 'pandas'],
+        'plotting':  ['matplotlib', 'bokeh==1.0.4', 'jinja2', 'pandas'],
     },
 
     # If there are data files included in your packages that need to be
